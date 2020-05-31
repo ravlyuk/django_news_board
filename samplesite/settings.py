297db75dad95bc8538f9496ec1cc251af4c1a1c2
eravlyuk <e.ravlyuk@gethatch.com>
2020-06-01 02:35:00 +0300

    added static folders

--- samplesite/settings.py
+++ samplesite/settings.py
@@ -117,7 +117,6 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
-    os.path.join(BASE_DIR, 'staticfiles'),
 )
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
