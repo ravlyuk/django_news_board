12bb73886ad3f0eb8a887f88f1b5ea77145f3d0b
eravlyuk <e.ravlyuk@gethatch.com>
2020-06-01 02:14:51 +0300

    added STATICFILES_STORAGE

--- samplesite/settings.py
+++ samplesite/settings.py
@@ -119,5 +119,5 @@ STATICFILES_DIRS = (
 )
 
 
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
