bd398098eff6ebbe89d5737367e09919ff70de11
eravlyuk <e.ravlyuk@gethatch.com>
2020-06-28 20:27:50 +0300

    for deploy

---
+++ samplesite/prod_settings.py
@@ -0,0 +1,20 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+SECRET_KEY = "1=8)^5ii(5*456_nb)rnrb2bfty9^==ypzlz@(+nh9%&i6gfhjfhj^6h*cex"
+
+DEBUG = False
+
+ALLOWED_HOSTS = ['127.0.0.1']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'db_name',
+        'USER': 'user_name',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
