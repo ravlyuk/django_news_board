66a475cfd19570baea3e41b68793fb881950ba0d
eravlyuk <e.ravlyuk@gethatch.com>
2020-06-29 20:11:18 +0300

    sqllite backup

--- samplesite/prod_settings.py
+++ samplesite/prod_settings.py
@@ -6,7 +6,7 @@ SECRET_KEY = "1=8)^5ii(5*456_nb)rnrb2bfty9^==ypzlz@(+nh9%&i6gfhjfhj^6h*cex"
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', '80.249.146.39']
 
 DATABASES = {
     'default': {
@@ -18,3 +18,7 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_DIR = os.path.join(BASE_DIR, 'static')
+# STATICFILES_DIRS = [STATIC_DIR]
--- samplesite/settings.py
+++ samplesite/settings.py
@@ -83,9 +83,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
-# STATICFILES_DIRS = [STATIC_DIR]
+
 
 
 MEDIA_URL = '/media/'
